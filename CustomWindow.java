af8ef13f780efa63c51e7b774af033398324f4b5
Kartikey Bhasin <68818281+Kartikey2235@users.noreply.github.com>
2020-08-22 01:29:29 +0530

    Add files via upload

---
+++ CustomWindow.java
@@ -0,0 +1,37 @@
+package UI;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.earthquakemap.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
+
+public class CustomWindow implements GoogleMap.InfoWindowAdapter {
+    private View view;
+    private LayoutInflater layoutInflater;
+    private Context context;
+
+    public CustomWindow(Context context){
+        this.context=context;
+
+        layoutInflater= (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        view=layoutInflater.inflate(R.layout.custom_window,null);
+    }
+    @Override
+    public View getInfoWindow(Marker marker) {
+        return null;
+    }
+
+    @Override
+    public View getInfoContents(Marker marker) {
+        TextView title=(TextView)view.findViewById(R.id.title);
+        title.setText(marker.getTitle());
+
+        TextView magnitude=(TextView)view.findViewById(R.id.magnitude);
+        magnitude.setText(marker.getSnippet());
+        return view;
+    }
+}
